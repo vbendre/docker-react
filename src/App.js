0456c25e9b0b7dd6073d4e05c776ea5574868031
vbendre <vbendre@ebates.com>
2020-04-15 11:36:06 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vishal React APP !
+          Vishal React APP - in AWS!
         </p>
         <a
           className="App-link"
